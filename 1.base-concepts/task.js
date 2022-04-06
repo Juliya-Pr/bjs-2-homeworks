0c3474ffaba3b309e0a463b9a4a3d86106b73627
Juliya-Pr <julee23@yandex.ru>
2022-04-07 00:46:56 +0300

    Выполненное задание №1 и исправленное №2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,35 +21,38 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount = [];
 
   let s = amount - contribution; //тело кредита
-  // const date1 = new Date(); //текущая дата
-  // const date2 = new Date(date); //срок погашения
-  //let n = ((date2 - date1) * 60 * 60 * 24 * 30);
-  //let percent; //процентная ставка
-  let n = 12;
-  let p = percent/12;
+  const date1 = new Date(); //текущая дата
+  const date2 = new Date(date); //срок погашения
+  let n = ((date2 - date1)/1000/60/60/24/30); //разница между датами в мс и пересчет в месяцы
+  let p = percent/12/100;
   let k = s * (p + (p / (((1 + p)**n) - 1)));
-  let r = k * 12;
-  // if (typeof percent === 'string' || typeof contribution === 'string' || typeof amount === 'string') {
-  //   Number(percent);
-  //   Number(contribution);
-  //   Number(amount);
-  //   console.log(`Общая сумма к выплате: ${Number(r.toFixed(2))}`);
-  //   console.log(`Первоначальный взнос: ${contribution}`);
-  //   console.log(`Погашение основного долга: ${s}`);
-  //   console.log(`Проценты за пользование кредитом: ${Number((r - s).toFixed(2))}`);
-  //   totalAmount[0] = (`Общая сумма к выплате: ${Number(r.toFixed(2))}`);
-  // } else
+  let r = Number((k * 12).toFixed(2));
+  console.log(`${r}`);
+  return(r);
 
-  if (typeof percent !== 'string') {
+  if (typeof percent === 'string') {
     return(`Параметр ${percentPlaceholder} содержит неправильное значение "${percent}"`);
-    //totalAmount[0] = (`Параметр "${percentPlaceholder}" содержит неправильное значение "${percent}"`);
-  } else if (typeof contribution === 'string') {
-    return(`Параметр "${contributionPlaceholder}" содержит неправильное значение "${contribution}"`);
-    //totalAmount[0] = (`Параметр "${contributionPlaceholder}" содержит неправильное значение "${contribution}"`);
-  } else if (typeof amount === 'string') {
-    return(`Параметр "${amountPlaceholder}" содержит неправильное значение "${amount}"`);
-    //totalAmount[0] = (`Параметр "${amountPlaceholder}" содержит неправильное значение "${amount}"`);
   }
+  // if (typeof percent === 'string' || typeof contribution === 'string' || typeof amount === 'string') {
+  //   percent = Number(percent);
+  //   contribution = Number(contribution);
+  //   amount = Number(amount);
+
+  // }
+
+
+  // if (typeof percent !== 'string') {
+  //
+  //   //totalAmount[0] = (`Параметр "${percentPlaceholder}" содержит неправильное значение "${percent}"`);
+  // } else if (typeof contribution === 'string') {
+  //   return(`Параметр "${contributionPlaceholder}" содержит неправильное значение "${contribution}"`);
+  //   //totalAmount[0] = (`Параметр "${contributionPlaceholder}" содержит неправильное значение "${contribution}"`);
+  // } else if (typeof amount === 'string') {
+  //   return(`Параметр "${amountPlaceholder}" содержит неправильное значение "${amount}"`);
+  //   //totalAmount[0] = (`Параметр "${amountPlaceholder}" содержит неправильное значение "${amount}"`);
+  // }
+
+
   // else {
   //   console.log(`Общая сумма к выплате: ${Number(r.toFixed(2))}`);
   //   console.log(`Первоначальный взнос: ${contribution}`);
