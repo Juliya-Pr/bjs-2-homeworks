db385e3522888aa3f050e9c01be04f174f8cd8c2
Juliya-Pr <julee23@yandex.ru>
2022-04-28 23:09:24 +0300

    Часть задания №1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  student1.subjectName = 'Algebra';
+  this.subjectName = 'Geometry';
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    student1.mark = 5;
+    this.mark = 2;
+  } else {
+    push.student1.mark = 4;
+    push.this.mark = 3;
+  }
+}
+
+Student.prototype.addMarks = function (mark1, mark2, mark3, ...rest) {
+  student1.mark = [5, 4, 5];
+  this.mark = [2, 3, 2];
+
+}
+
+Student.prototype.getAverage = function () {
+
+}
+
+Student.prototype.exclude = function (reason) {
+
+}
